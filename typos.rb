b555f4a514176bc9542c0fb290fda8220b19eef5
Kat Hicks <kmhicks92@gmail.com>
2016-12-06 13:46:20 +0000

    Corrected hash notation as it's appended into students

--- typos.rb
+++ typos.rb
@@ -8,8 +8,8 @@ def input_students
   % while the name is not empty, repeat this code
   when !name.empty? do
       % add the student hash to the array
-      students << {:name : name :cohort : :november}
-      puts "Now we have #{student.count} students"
+      students << {name: name, cohort: :november}
+      puts "Now we have #{students.count} students"
       % get another name from the user
       name = get.chomp
   end
